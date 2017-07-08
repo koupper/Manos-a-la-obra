8627a34623af78816b8093dba41dd27c2a6a8fba
Brandon González <bgonzalezg1505@alumno.ipn.mx>
2017-07-08 02:27:36 -0500

    Add files via upload
    Barra de estado para tiempo de desastres.

---
+++ Manos a la obra/Assets/scripts/BarraEstado.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraEstado : MonoBehaviour {
+
+    float timelife, maxTime = 100f, time;
+    public Image stateBar;
+	// Use this for initialization
+	void Start () {
+        timelife = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer(time = 1 * Time.deltaTime);
+	}
+
+    public void Timer(float amount)
+    {
+        timelife = Mathf.Clamp(timelife - amount, 0f, maxTime);
+        stateBar.transform.localScale = new Vector2(timelife/maxTime, 0.8101366f);
+    }
+}
